17d198f51020c3119f5c34be7c51e2f0cec50b7f
slopezz <sergio-lopez.mateos@gmail.com>
2018-07-22 14:10:47 +0000

    Fix gcloud zone id and fix docker tag/push command

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,10 @@ gcloud --quiet config set container/cluster $CLUSTER_NAME
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials $CLUSTER_NAME
 
-gcloud docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}
 
-yes | gcloud beta container images add-tag gcr.io/${PROJECT_NAM}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest
+docker tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest
+gcloud auth configure-docker
+docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}
 
 kubectl config view
 kubectl config current-context

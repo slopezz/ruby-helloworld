05ac4cd61183014fe76b628301a7e01499223117
slopezz <sergio-lopez.mateos@gmail.com>
2018-07-23 15:11:24 +0000

    Add Makefile for docker image build/push + k8s-deploy instead of using all commands on deploy bash script

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,4 @@ set -e
 
 cd app
 
-docker build -t gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT .
-
-gcloud auth activate-service-account --key-file "${GOOGLE_APPLICATION_CREDENTIALS}"
-
-gcloud --quiet config set project $PROJECT_NAME
-gcloud --quiet config set container/cluster $CLUSTER_NAME
-gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
-gcloud --quiet container clusters get-credentials $CLUSTER_NAME
-
-
-docker tag gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:latest
-gcloud auth configure-docker
-docker push gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}
-
-kubectl config view
-kubectl config current-context
-
-kubectl set image deployment/${KUBE_DEPLOYMENT_NAME} ${KUBE_DEPLOYMENT_CONTAINER_NAME}=gcr.io/${PROJECT_NAME}/${DOCKER_IMAGE_NAME}:$TRAVIS_COMMIT
+make deploy
